211de46fe4ccf3f83796dfa4872c3bce46ace39c
Catherine <kankatya@bk.ru>
2022-08-27 13:49:00 +0500

    Updated task 43

--- task 43/Program.cs
+++ task 43/Program.cs
@@ -1,8 +1,12 @@
-﻿Console.Write("Введите коэффиценты первой прямой: ");
+﻿Console.WriteLine("Введите коэффиценты первой прямой: ");
+Console.Write("k1: ");
 float k1= Convert.ToInt32(Console.ReadLine());
+Console.Write("b1: ");
 float b1= Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите коэффиценты второй прямой: ");
+Console.WriteLine("Введите коэффиценты второй прямой: ");
+Console.Write("k2: ");
 float k2= Convert.ToInt32(Console.ReadLine());
+Console.Write("b2: ");
 float b2= Convert.ToInt32(Console.ReadLine());
 float x = -((b1-b2)/(k1-k2));
 float y = k1*x + b1;
