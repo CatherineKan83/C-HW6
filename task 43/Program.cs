7de6b6887af40eaba54ac4c6e53c4830a7b1bb25
Catherine <kankatya@bk.ru>
2022-08-27 13:44:40 +0500

    Added task 43

---
+++ task 43/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите коэффиценты первой прямой: ");
+float k1= Convert.ToInt32(Console.ReadLine());
+float b1= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите коэффиценты второй прямой: ");
+float k2= Convert.ToInt32(Console.ReadLine());
+float b2= Convert.ToInt32(Console.ReadLine());
+float x = -((b1-b2)/(k1-k2));
+float y = k1*x + b1;
+if(k1!=k2)
+{
+    Console.WriteLine($"Точка пересечения прямых - [{x}, {y}] .");
+}
+else
+{
+    Console.WriteLine("Прямые с данными параметрами параллельны.");
+}
